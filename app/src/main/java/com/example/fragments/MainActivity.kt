291b9e2e3a652b33ca470a2cd60f95f231abed6c
SammyCalle <sammycallet@gmail.com>
2020-04-03 19:50:58 -0500

    second commit

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.addOnBackStackChangedListener {
             textViewFragmentCount.setText("Fragment count in back stack: "+supportFragmentManager.backStackEntryCount)
+            val backstackentryMessage = StringBuilder("Current status of fragment transaction back stack: " + supportFragmentManager.backStackEntryCount+"\n")
+            for(index in supportFragmentManager.backStackEntryCount-1 downTo 0){
+
+            }
         }
         val buttonAddFragment = findViewById<Button>(R.id.buttonAddFragment);
         buttonAddFragment.setOnClickListener {
@@ -33,7 +38,19 @@ class MainActivity : AppCompatActivity() {
         val fm = supportFragmentManager
         val ft = fm.beginTransaction()
         ft.add(R.id.fragmentContainer,fragment)
-            .addToBackStack("fragmentStack1")
+//            .addToBackStack("fragmentStack1")
             .commit()
+    }
+
+    override fun onBackPressed() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer);
+        if (fragment!=null){
+            val ft = supportFragmentManager.beginTransaction()
+            ft.remove(fragment)
+                .addToBackStack(null)
+                .commit()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
