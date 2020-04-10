4e05a69ae95310485116f5b7bcf75f3a93d92eee
sacalle <sammycallet@gmail.com>
2020-04-10 10:27:02 -0500

    Example Finished

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,47 +1,108 @@
 package com.example.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.StringBuilder
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val  COMMON_TAG:String = "CombinedLifeCycle"
+    private val ACTIVITY_NAME = MainActivity::class.java.simpleName
+    private val TAG = ACTIVITY_NAME
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.addOnBackStackChangedListener {
-            textViewFragmentCount.setText("Fragment count in back stack: "+supportFragmentManager.backStackEntryCount)
-            val backstackentryMessage = StringBuilder("Current status of fragment transaction back stack: " + supportFragmentManager.backStackEntryCount+"\n")
-            for(index in supportFragmentManager.backStackEntryCount-1 downTo 0){
 
+
+
+
+
+        val fm = supportFragmentManager
+        textViewFragmentCount.setText("Fragment count in back stack: "+fm.getBackStackEntryCount());
+        supportFragmentManager.addOnBackStackChangedListener {
+            textViewFragmentCount.setText("Fragment count in back stack: "+fm.backStackEntryCount)
+            val backstackentryMessage = StringBuilder("Current status of fragment transaction back stack: "
+                    + fm.backStackEntryCount+"\n")
+            for(index in supportFragmentManager.backStackEntryCount-1 downTo 0){
+                val backStackEntry =  fm.getBackStackEntryAt(index)
+                backstackentryMessage.append(backStackEntry.getName()+"\n")
             }
+            Log.i(TAG,backstackentryMessage.toString())
         }
-        val buttonAddFragment = findViewById<Button>(R.id.buttonAddFragment);
-        buttonAddFragment.setOnClickListener {
-            addFragment()
-        }
+        Log.i(COMMON_TAG,"Initial BackStackEntryCount: "+fm.getBackStackEntryCount());
+
+        buttonAddFragmentOne.setOnClickListener(this);
+        buttonPopFragment.setOnClickListener(this);
+        buttonRemoveFragment.setOnClickListener(this);
+
     }
 
     private fun addFragment () : Unit {
-        val fragment : Fragment
-        when(supportFragmentManager.backStackEntryCount){
-            0 ->  fragment = SampleFragment()
-            1 ->  fragment = FragmentTwo()
-            2 ->  fragment = FragmentThree()
-            else -> fragment = SampleFragment()
+        val fragment : Fragment?
+        if (supportFragmentManager.backStackEntryCount > 0){
+            when(supportFragmentManager.backStackEntryCount){
+                0 ->  loadFragmentOne()
+                1 ->  loadFragmentTwo()
+                2 ->  loadFragmentThree()
+                else -> loadFragmentOne()
+            }
+        }else{
+            fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+            if(fragment is SampleFragment){
+                loadFragmentTwo();
+            }else if(fragment is FragmentTwo){
+                loadFragmentThree();
+            }else if(fragment is FragmentThree){
+                loadFragmentOne();
+            }else{
+                loadFragmentOne();
+            }
         }
-        val fm = supportFragmentManager
-        val ft = fm.beginTransaction()
-        ft.add(R.id.fragmentContainer,fragment)
-//            .addToBackStack("fragmentStack1")
-            .commit()
+    }
+    private fun loadFragmentOne() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var fragment: Fragment
+        /*if(fragment!=null){
+            fragmentTransaction.remove(fragment);
+        }*/
+        fragment = SampleFragment()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment, "demofragment")
+        fragmentTransaction.addToBackStack("Add $fragment")
+        fragmentTransaction.commit()
     }
 
+    private fun loadFragmentTwo() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var fragment: Fragment
+        /*if(fragment!=null){
+            fragmentTransaction.remove(fragment);
+        }*/
+        fragment = FragmentTwo()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment, "demofragment")
+        fragmentTransaction.addToBackStack("Add $fragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun loadFragmentThree() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var fragment: Fragment
+        /*if(fragment!=null){
+            fragmentTransaction.remove(fragment);
+        }*/fragment = FragmentThree()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment, "demofragment")
+        fragmentTransaction.addToBackStack("Add $fragment")
+        fragmentTransaction.commit()
+    }
     override fun onBackPressed() {
         val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer);
         if (fragment!=null){
@@ -51,6 +112,32 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }else{
             super.onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            buttonAddFragmentOne -> addFragment()
+//            buttonPopFragment-> supportFragmentManager.popBackStack()
+//            buttonPopFragment-> supportFragmentManager.popBackStack(0,0)
+            buttonPopFragment -> supportFragmentManager.popBackStack(
+                "Add SampleFragment",
+                FragmentManager.POP_BACK_STACK_INCLUSIVE
+            )
+            buttonRemoveFragment -> {
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+                if (fragment != null) {
+                    fragmentTransaction.remove(fragment)
+                    fragmentTransaction.addToBackStack("Remove $fragment")
+                    fragmentTransaction.commit()
+                } else {
+                    Toast.makeText(this, "No Fragment to remove", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                
+            }
         }
     }
 }
